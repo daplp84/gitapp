83b05c4e7e1605b4117a04ae59daf3f3314379a5
Daniel Piassini <danielpiassini@hotmail.com>
2021-05-12 23:28:23 -0300

    Soluciona problema de fecha al editar un movimiento

--- server/models/movement.js
+++ server/models/movement.js
@@ -96,7 +96,6 @@ const updateMovement = (
 ) => {
     return Movement.findOne({ where: { id: id } }).then((movement) => {
         if (movement != null) {
-            date = new Date();
             return movement.update({ date, amount, type, category, description });
         }
         return null;
