d5f250475e3d77410ba644b91dc9a854c8e87f4b
rosanabj <rosanabajcic@gmail.com>
2021-05-30 21:28:08 -0300

    Se agrega test para validar que se guarde correctamente el campo descripción. Y se agrega otro test que valida se muestre este campo al editar un egreso.

--- tests/e2e/specs/expense.spec.js
+++ tests/e2e/specs/expense.spec.js
@@ -14,4 +14,29 @@ describe('Egresos Test', () => {
 
         cy.get('[data-testid=movement]').should('have.length', 6);
     });
+
+    it('Deberia poder crear un nuevo egreso con descripcion', () => {
+        cy.visit('/expense');
+
+        cy.get('input[name=description]').type('Cuadernos');
+        cy.get('input[name=date]').type('2021-05-30');
+        cy.get('input[name=category]').type('Libreria');
+        cy.get('input[name=amount]').type('300');
+        cy.contains('Guardar').click();
+
+        cy.get('[data-testid=movement]').should('have.length', 5);
+    });
+
+
+    it('Deberia visualizar el campo descripcion al editar un egreso', () => {
+        cy.visit('/expense');
+
+        cy.get('[data-testid=movement]')
+            .find('button')
+            .contains('editar')
+            .click();
+
+        cy.get('input[name=description]');
+    });
+
 });
